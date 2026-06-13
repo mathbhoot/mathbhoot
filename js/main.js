aaa39a1c5ebf919e3e3e1cec8b9963798fc16297
mathbhoot <mathbhoot.sakshi@gmail.com>
2026-06-13 13:17:35 +0530

    Update main.js
    cursor

--- js/main.js
+++ js/main.js
@@ -81,3 +81,49 @@ title.addEventListener('mouseout', () => {
     }
 
 }
+
+// existing code
+
+// existing code
+
+// existing code
+
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+    createSpark(e.clientX, e.clientY);
+});
+
+function createSpark(x,y){
+
+    const spark = document.createElement("div");
+
+    spark.classList.add("spark");
+
+    const colors = [
+        "#8b5cf6",
+        "#60a5fa",
+        "#22d3ee",
+        "#fbbf24"
+    ];
+
+    spark.style.background =
+        colors[Math.floor(Math.random()*colors.length)];
+
+    spark.style.left =
+        (x + (Math.random()*20-10)) + "px";
+
+    spark.style.top =
+        (y + (Math.random()*20-10)) + "px";
+
+    document.body.appendChild(spark);
+
+    setTimeout(()=>{
+        spark.remove();
+    },1000);
+}
